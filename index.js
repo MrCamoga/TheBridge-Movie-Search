73598f1f61a9c8fc761fa7d21a8b53689ce3b74f
MrCamoga <mrcamoga@gmail.com>
2025-04-21 23:10:31 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const form = document.getElementById("searchform");
+const APITOKEN = ""
+const config = {headers: {Authorization: `Bearer ${APITOKEN}`}};
+
+form.addEventListener("submit", submitForm)
+
+
+async function submitForm(event) {
+    event.preventDefault();
+    const query = this.movie.value;
+    const movieContainer = document.getElementById("movie-container")
+    movieContainer.innerHTML = ""; // reset container
+
+
+    const genres = await getGenres();
+    console.log(genres)
+
+    try {
+        const movies = await axios.get(`https://api.themoviedb.org/3/search/movie?query=${query}`, config);
+        console.log(genres)
+        movies.data.results.forEach(movie => {
+            const {title,overview,poster_path,genre_ids} = movie;
+            movieContainer.innerHTML += movieCardTemplate(title,overview,poster_path,genre_ids.map(id => genres[id]))
+        });
+    } catch(error) {
+        console.error(error);
+    }
+}
+
+async function getGenres() {
+    try {
+        const response = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?language=en`, config);
+        return Object.fromEntries(response.data.genres.map(genre => [+genre.id, genre.name]))
+    } catch(error) {
+        console.error(error);
+    }
+}
+
+function movieCardTemplate(title, descr, img, genres) {
+    return `
+    <div class="movie-card">
+        <img src="https://image.tmdb.org/t/p/original${img}" alt="movie cover">
+        <div>
+            <div class="movie-info">
+                <h2>${title}</h2>
+                <p>${descr}</p>
+            </div>
+            <div class="movie-genre-container">
+                ${genres.map(genre => `<span class="movie-genre">${genre}</span>`).join("")}
+            </div>
+        </div>
+    </div>`
+}
